d054aaf5d548c480e145a2561313a8370d777edf
Marc <a1627644@student.adelaide.edu.au>
2015-01-07 09:23:07 +1030

    Show journal entries in Android app profile view.

--- grails/ConnectedHealth/grails-app/controllers/connectedhealth/JournalEntryController.groovy
+++ grails/ConnectedHealth/grails-app/controllers/connectedhealth/JournalEntryController.groovy
@@ -7,28 +7,29 @@ class JournalEntryController {
 
     def scaffold = JournalEntry
 
-    def showEntry() {
-
+    // get journal entries for a single patient
+    // (required) patientID = ID of patient to get journal entries for
+    def entries() {
         if (params.patientID == null) {
             render "Patient ID is required"
             return
         }
-        JournalEntry[] patientJournalEntry = JournalEntry.findAllByPatientID(params.patientID)
-        // get all notes for this patient
+
+        JournalEntry[] patientJournalEntry = JournalEntry.findAllByPatientID(params.patientID)  // get all notes for this patient
 
         // return notes in JSON format, with patient ID removed
-        ArrayList<JSONObject> JournalEntryToSend = new ArrayList<JSONObject>()
+        ArrayList<JSONObject> JournalEntriesToSend = new ArrayList<JSONObject>()
 
         for (int i = 0; i < patientJournalEntry.size(); i++) {
             JSONObject journalEntry = new JSONObject()
             journalEntry.put("ID", patientJournalEntry[i].getId())
-            journalEntry.put("content", patientJournalEntry[i].getCreated())
-            journalEntry.put("created", patientJournalEntry[i].getContent())
-            journalEntry.put("updated", patientJournalEntry[i].getContent())
-            JournalEntryToSend.add(journalEntry)
+            journalEntry.put("content", patientJournalEntry[i].getContent())
+            journalEntry.put("created", patientJournalEntry[i].getCreated())
+            journalEntry.put("updated", patientJournalEntry[i].getUpdated())
+            JournalEntriesToSend.add(journalEntry)
         }
 
-        JSONArray JournalEntryJson = new JSONArray(JournalEntryToSend)
+        JSONArray JournalEntryJson = new JSONArray(JournalEntriesToSend)
         render JournalEntryJson.toString()
     }
 
