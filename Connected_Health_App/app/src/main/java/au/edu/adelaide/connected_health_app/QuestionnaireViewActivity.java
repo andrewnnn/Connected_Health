90344e7e3aee9e3ab8a674b8ab243be46563132a
thatscool <a1627698@student.adelaide.edu.au>
2015-01-08 14:03:21 +1030

    resolve

--- Connected_Health_App/app/src/main/java/au/edu/adelaide/connected_health_app/QuestionnaireViewActivity.java
+++ Connected_Health_App/app/src/main/java/au/edu/adelaide/connected_health_app/QuestionnaireViewActivity.java
@@ -5,14 +5,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+<<<<<<< HEAD
 import android.view.View;
 import android.widget.TextView;
+=======
+import android.widget.TextView;
+
+>>>>>>> master
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+<<<<<<< HEAD
+=======
+
+>>>>>>> master
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
