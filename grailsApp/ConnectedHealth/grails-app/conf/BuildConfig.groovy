2aca08da8edfb7ce65ce50d3f0c4035d26708caf
Marc <a1627644@student.adelaide.edu.au>
2015-01-19 14:36:17 +1030

    Update patient show route

--- grailsApp/ConnectedHealth/grails-app/conf/BuildConfig.groovy
+++ grailsApp/ConnectedHealth/grails-app/conf/BuildConfig.groovy
@@ -62,6 +62,7 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
         compile ":asset-pipeline:1.9.9"
+        compile ":fields:1.4"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
