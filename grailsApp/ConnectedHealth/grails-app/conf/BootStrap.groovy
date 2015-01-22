c5947c6b1faae5607f88326642b1930cd8626809
Marc <a1627644@student.adelaide.edu.au>
2015-01-22 12:23:59 +1030

    Realistic data in DB seed

--- grailsApp/ConnectedHealth/grails-app/conf/BootStrap.groovy
+++ grailsApp/ConnectedHealth/grails-app/conf/BootStrap.groovy
@@ -5,14 +5,15 @@ import connectedhealth.MedicalNote
 import connectedhealth.Patient
 import connectedhealth.Question
 import connectedhealth.Questionnaire
+import org.joda.time.DateTime
 
 class BootStrap {
 
     def init = { servletContext ->
 
-        Patient p1 = new Patient(firstName: "John", lastName: "Smith", homeAddress: "123 Example St Fakeville", phone: "12340987", contactEmail: "john@smith.ru")
-        Patient p2 = new Patient(firstName: "John2", lastName: "Smith2", homeAddress: "123 Example St Fakeville2", phone: "123409872", contactEmail: "john2@smith.ru")
-        Patient p3 = new Patient(firstName: "John3", lastName: "Smith3", homeAddress: "123 Example St Fakeville3", phone: "123409873", contactEmail: "john3@smith.ru")
+        Patient p1 = new Patient(firstName: "Wayland", lastName: "Johannes", homeAddress: "123 Example St Fakeville", phone: "12340987", contactEmail: "wayland@johannes.com")
+        Patient p2 = new Patient(firstName: "Julian", lastName: "Leandro", homeAddress: "5 Example St Fakeland", phone: "22221111", contactEmail: "julian@leandro.com")
+        Patient p3 = new Patient(firstName: "John", lastName: "Smith", homeAddress: "99 Example St Faketown", phone: "64644646", contactEmail: "john3@smith.com")
         p1.save()
         p2.save()
         p3.save()
@@ -29,128 +30,117 @@ class BootStrap {
         p3.addToMeasurementTypes(mt1).addToMeasurementTypes(mt2).addToMeasurementTypes(mt3).save()
 
         //create database seed
-        MedicalNote a = new MedicalNote(patient: p1, content: "Begin seven day trial of meloxicam 15mg PO daily. Patient given instruction sheet on back exercises to stretch " +
-                "injured muscles and advised to continue work and normal activity as tolerated. Patient advised to keep a pillow between her legs" +
-                " when sleeping on side. Patient advised to avoid heavy lifting until back heals. Follow up in 4-6 weeks or sooner if pain worsens or new symptoms develop.", created: new Date(1992,1,1,1,1))
-        a.save()
+        new MedicalNote(patient: p1, content: "Use the instruction sheet to stretch injured muscles.",
+                created: new DateTime(2015,1,26,1,1,13).toDate()).save()
 
-        a = new MedicalNote(patient: p2, content: "Suggest stopping any further doses of imipenem/cilastin. Select other antibiotic to cover gram neg rods until C/S" +
-                " is available. Options include: aztreonam, cefipime, ceftazidime, possibly ciprofloxacin.", created: new Date(1540,8,2,6,4,6))
-        a.save()
+        new MedicalNote(patient: p1, content: "Avoid heavy lifting for the next 2 weeks.",
+                created: new DateTime(2015,1,25,1,1,8).toDate()).save()
 
-        a = new MedicalNote(patient: p2, content: "Physical therapy, to help you keep proper posture, and deep breathing exercises, to enhance your lung capacity. " +
-                "A physical therapist can also help you learn to use heat and cold to help control your pain and stiffness. Heat can help with" +
-                " relaxation and pain relief, and cold can help reduce inflammation.", created: new Date(2235,8,2,6,4,6))
-        a.save()
+        new MedicalNote(patient: p2, content: "Your weight has improved since you started doing regular exercise.",
+                created: new DateTime(2015,1,25,1,1,14).toDate()).save()
 
-        a = new MedicalNote(patient: p3, content: "I am the third note", created: new Date(2066,4,2,33,4,5))
-        a.save()
+        new MedicalNote(patient: p2, content: "Remember to stretch before exercising.",
+                created: new DateTime(2015,1,21,1,1,25).toDate()).save()
 
-        a = new MedicalNote(patient: p3, content: "Please pay the medical fee", created: new Date(3135,12,2,3,35,5))
-        a.save()
+        new MedicalNote(patient: p2, content: "Continue your physical therapy to help you keep proper posture, and deep breathing exercises to enhance your lung capacity.",
+                created: new DateTime(2015,1,20,1,1,20).toDate()).save()
 
-        a = new MedicalNote(patient: p3, content: "NO FEE NO LIFE", created: new Date(1234,1,12,3,4,5))
-        a.save()
+        new MedicalNote(patient: p2, content: "Your weight has seen an increase in recent weeks, please stick to the diet prescribed or come into the clinic for more consultation.",
+                created: new DateTime(2015,1,18,6,4,6).toDate()).save()
 
-        a = new MedicalNote(patient: p3, content: "FEE! FEE! FEE! FEE! FEE! FEE!", created: new Date(7654,1,12,3,4,5))
-        a.save()
+        new MedicalNote(patient: p3, content: "Please sleep earlier.",
+                created: new DateTime(2015,1,25,23,4,5).toDate()).save()
 
-        a = new MedicalNote(patient: p3, content: "If you don't pay the fee today, I will ask someone to kill you tomorrow", created: new Date(9999,1,12,3,4,5))
-        a.save()
+        new MedicalNote(patient: p3, content: "Advise you to stick to 3-4 small meals a day to keep weight levels stable.",
+                created: new DateTime(2015,1,24,3,35,5).toDate()).save()
 
-        a = new MedicalNote(patient: p3, content: "good boy", created: new Date(9999,9,9,9,9,9))
-        a.save()
+        new MedicalNote(patient: p3, content: "Remember to take medicine in the morning, afternoon and at night.",
+                created: new DateTime(2015,1,23,3,4,5).toDate()).save()
 
+        new MedicalNote(patient: p3, content: "Taking your daily medication after meals is recommended.",
+                created: new DateTime(2015,1,22,3,4,5).toDate()).save()
+
+        new MedicalNote(patient: p3, content: "Advise you to fit in a minimum of 30 mins of physical exercise per day.",
+                created: new DateTime(2015,1,21,3,4,5).toDate()).save()
+
+        new MedicalNote(patient: p3, content: "Make sure you have a decent stretch before commencing physical exercise.",
+                created: new DateTime(2015,1,20,9,9,9).toDate()).save()
 
         //journal entry
-        JournalEntry b = new JournalEntry(patient: p1, content: "This is my small Journal", created: new Date(), updated: new Date())
-        b.save()
+        new JournalEntry(patient: p1, content: "I started going for walks in the afternoon.",
+                created: new DateTime(2015,1,25,19,30,30).toDate(), updated: new DateTime(2015,1,25,19,30,30).toDate()).save()
 
-        b = new JournalEntry(patient: p1, content: "Feeling GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOD today!!!", created: new Date(), updated: new Date())
-        b.save()
+        new JournalEntry(patient: p1, content: "My legs are a bit sore after I walked quickly yesterday. I think it's because I didn't stretch before walking.",
+                created: new DateTime(2015,1,25,18,30,30).toDate(), updated: new DateTime(2015,1,25,18,30,30).toDate()).save()
 
-        b = new JournalEntry(patient: p1, content: "Feeling BAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD today!!!", created: new Date(), updated: new Date())
-        b.save()
+        new JournalEntry(patient: p1, content: "I'm getting used to writing daily journal entries.",
+                created: new DateTime(2015,1,24,13,10,5).toDate(), updated: new DateTime(2015,1,24,13,10,5).toDate()).save()
 
-        b = new JournalEntry(patient: p2, content: "someone gonna kill me tomorrow, I will run away now!!!", created: new Date(), updated: new Date())
-        b.save()
+        new JournalEntry(patient: p1, content: "I forgot to answer the questionnaires yesterday.",
+                created: new DateTime(2015,1,22,10,10,9).toDate(), updated: new DateTime(2015,1,22,10,10,9).toDate()).save()
 
-        b = new JournalEntry(patient: p3, content: "I am a rich man, I have many houses", created: new Date(), updated: new Date())
-        b.save()
+        new JournalEntry(patient: p2, content: "I've felt more energetic since I started taking my new medication.",
+                created: new DateTime(2015,1,25,12,11,10).toDate(), updated: new DateTime(2015,1,25,12,11,10).toDate()).save()
 
-        b = new JournalEntry(patient: p3, content: "and many cars!!!", created: new Date(), updated: new Date())
-        b.save()
+        new JournalEntry(patient: p3, content: "Today was a good day, I reached a new weekly record for steps taken.",
+                created: new DateTime(2015,1,25,11,11,10).toDate(), updated: new DateTime(2015,1,25,11,11,10).toDate()).save()
 
-        b = new JournalEntry(patient: p3, content: "and many banks!!!", created: new Date(), updated: new Date())
-        b.save()
+        new JournalEntry(patient: p3, content: "I can already see the difference in weight from striving to one up my previous steps from the day before.",
+                created: new DateTime(2015,1,25,10,10,10).toDate(), updated: new DateTime(2015,1,25,10,10,10).toDate()).save()
 
-        b = new JournalEntry(patient: p3, content: "and many boats!!!", created: new Date(), updated: new Date())
-        b.save()
+        new JournalEntry(patient: p3, content: "After a few weeks, I am really getting the hang of this system and my carers are also seeing improvements. I am really happy!",
+                created: new DateTime(2015,1,24,12,10,5).toDate(), updated: new DateTime(2015,1,24,12,10,5).toDate()).save()
 
-        b = new JournalEntry(patient: p3, content: "and many many many many dogs!!!", created: new Date(), updated: new Date())
-        b.save()
+        new JournalEntry(patient: p3, content: "My mood has been better in recent weeks. I think this can be attributed to the clean diet and daily exercise, which I'm really enjoying.",
+                created: new DateTime(2015,1,23,12,10,5).toDate(), updated: new DateTime(2015,1,23,12,10,5).toDate()).save()
 
-        Questionnaire questionnaire1 = new Questionnaire(name: "StandardQuestionnaire", description: "General questions for no specific condition.")
+        new JournalEntry(patient: p3, content: "Yesterday I received my first remote check up from a nurse. It was nice to have an expert be able to check up and discuss my current health.",
+                created: new DateTime(2015,1,22,10,10,5).toDate(), updated: new DateTime(2015,1,22,10,10,5).toDate()).save()
+
+        Questionnaire questionnaire1 = new Questionnaire(name: "General Questionnaire", description: "Questions about general wellbeing.")
         questionnaire1.save()
 
-        Question q1 = new Question(content: "q1", answerFormat: 0, questionnaire: questionnaire1)
-        q1.save()
-        Question q2 = new Question(content: "q2", answerFormat: 1, questionnaire: questionnaire1)
-        q2.save()
-        Question q3 = new Question(content: "q3", answerFormat: 2, questionnaire: questionnaire1)
-        q3.save()
+        Question q1q1 = new Question(content: "Do you feel tired today?", answerFormat: 0, questionnaire: questionnaire1)
+        q1q1.save()
+        new Choice(content: "Yes", question: q1q1).save()
+        new Choice(content: "A bit", question: q1q1).save()
+        new Choice(content: "No", question: q1q1).save()
 
-        Choice q1c1 = new Choice(content: "choice1 for q1", question: q1)
-        q1c1.save()
-        Choice q1c2 = new Choice(content: "choice2 for q1", question: q1)
-        q1c2.save()
-        Choice q1c3 = new Choice(content: "choice3 for q1", question: q1)
-        q1c3.save()
+        Question q1q2 = new Question(content: "How good was your sleep last night?", answerFormat: 0, questionnaire: questionnaire1)
+        q1q2.save()
+        new Choice(content: "Very good", question: q1q2).save()
+        new Choice(content: "Good", question: q1q2).save()
+        new Choice(content: "Ok", question: q1q2).save()
+        new Choice(content: "Bad", question: q1q2).save()
+        new Choice(content: "Very bad", question: q1q2).save()
 
-        Choice q2c1 = new Choice(content: "choice1 for q2", question: q2)
-        q2c1.save()
-        Choice q2c2 = new Choice(content: "choice2 for q2", question: q2)
-        q2c2.save()
-        Choice q2c3 = new Choice(content: "choice3 for q2", question: q2)
-        q2c3.save()
+        Question q1q3 = new Question(content: "Did you take your medication today?", answerFormat: 0, questionnaire: questionnaire1)
+        q1q3.save()
+        new Choice(content: "Yes", question: q1q3).save()
+        new Choice(content: "No", question: q1q3).save()
+
+        Question q1q4 = new Question(content: "Do you have difficulty with any of the following activities?", answerFormat: 1, questionnaire: questionnaire1)
+        q1q4.save()
+        new Choice(content: "Standing up after sitting", question: q1q4).save()
+        new Choice(content: "Sitting down up after standing", question: q1q4).save()
+        new Choice(content: "Walking", question: q1q4).save()
 
         Questionnaire questionnaire2 = new Questionnaire(name: "Physical Activity Questionnaire", description: "Questions about recent physical activity.")
         questionnaire2.save()
 
-        Question q4 = new Question(content: "q4", answerFormat: 1, questionnaire: questionnaire2)
-        q4.save()
+        Question q2q1 = new Question(content: "Did you do 30 minutes of exercise yesterday?", answerFormat: 0, questionnaire: questionnaire2)
+        q2q1.save()
+        new Choice(content: "Yes", question: q2q1).save()
+        new Choice(content: "No", question: q2q1).save()
 
-        for (int i = 1; i <= 10; i++) {
-            Choice q4choice = new Choice(content: "choice" + i + " for q4", question: q4);
-            q4choice.save();
-        }
-
-        Question q5 = new Question(content: "q5", answerFormat: 0, questionnaire: questionnaire2)
-        q5.save()
-
-        for (int i = 1; i <= 5; i++) {
-            Choice q5choice = new Choice(content: "choice" + i + " for q5", question: q5);
-            q5choice.save();
-        }
-
-        Question q6 = new Question(content: "q6", answerFormat: 0, questionnaire: questionnaire2)
-        q6.save()
-
-        for (int i = 1; i <= 5; i++) {
-            Choice q6choice = new Choice(content: "choice" + i + " for q6", question: q6);
-            q6choice.save();
-        }
-
-        Question q7 = new Question(content: "q7", answerFormat: 0, questionnaire: questionnaire2)
-        q7.save()
-
-        for (int i = 1; i <= 5; i++) {
-            Choice q7choice = new Choice(content: "choice" + i + " for q7", question: q7);
-            q7choice.save();
-        }
-
-
-
+        Question q2q2 = new Question(content: "Do you have difficulty with any of the following activities?", answerFormat: 1, questionnaire: questionnaire2)
+        q2q2.save()
+        new Choice(content: "Standing up after sitting", question: q2q2).save()
+        new Choice(content: "Sitting down after standing", question: q2q2).save()
+        new Choice(content: "Concentrating on what you are doing", question: q2q2).save()
+        new Choice(content: "Turning your head", question: q2q2).save()
+        new Choice(content: "Walking", question: q2q2).save()
+        new Choice(content: "Writing", question: q2q2).save()
 
     }
     def destroy = {
