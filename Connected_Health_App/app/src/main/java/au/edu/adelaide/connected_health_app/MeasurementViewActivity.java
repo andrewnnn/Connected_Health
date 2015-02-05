d81929ba86fe6e81560a2ac533c2666f87c536ad
Marc <a1627644@student.adelaide.edu.au>
2015-02-05 11:15:03 +1030

    Use new TextPreview class to do setup for measurement view activity. set meas icon in preview heading

--- Connected_Health_App/app/src/main/java/au/edu/adelaide/connected_health_app/MeasurementViewActivity.java
+++ Connected_Health_App/app/src/main/java/au/edu/adelaide/connected_health_app/MeasurementViewActivity.java
@@ -18,32 +18,20 @@ import java.util.ArrayList;
 public class MeasurementViewActivity extends TextPreview {
 
     private final int patientID = 1;
-    private ArrayList<JSONObject> measurementTypesForPreviews;
-    private int pageNumber = -1;
-    private final int textPreviewsPerPage = HelperSingleton.getInstance().getTextPreviewsPerPage();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.generic_text_preview_view);
 
-        PatientSingleton.getInstance().setCurrentItemType(PatientSingleton.ItemType.measurement);
-        removeUnusedNavButtons();
+        textPreviewSetup(PatientSingleton.ItemType.measurement);
 
         try {
-            if (getIntent().hasExtra("pageNumber")) {
-                pageNumber = getIntent().getExtras().getInt("pageNumber");
-                measurementTypesForPreviews = PatientSingleton.getInstance().getMeasurementTypes(pageNumber*textPreviewsPerPage,pageNumber*textPreviewsPerPage + 2);
-            } else {
-                measurementTypesForPreviews = PatientSingleton.getInstance().getMeasurementTypes(0,2);
-                pageNumber = 0;
-            }
-
             // for each preview, set background colour to match home panel and set preview text
             int i;
-            for (i = 0; i < measurementTypesForPreviews.size(); i++){
-                String name = measurementTypesForPreviews.get(i).getString("name");
-                String description = measurementTypesForPreviews.get(i).getString("description");
+            for (i = 0; i < itemsForPreviews.size(); i++){
+                String name = itemsForPreviews.get(i).getString("name");
+                String description = itemsForPreviews.get(i).getString("description");
                 String preview = name + "\n\n" + description;
 
                 int resID = getResources().getIdentifier("preview_text" + i,
@@ -103,44 +91,6 @@ public class MeasurementViewActivity extends TextPreview {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void goToSingleItemView(View view) {
-        int itemPageOffset = -1;
-        switch(view.getId()) {
-            case R.id.preview0:
-                PatientSingleton.getInstance().setCurrentObject(measurementTypesForPreviews.get(0));
-                itemPageOffset = 0;
-                break;
-            case R.id.preview1:
-                PatientSingleton.getInstance().setCurrentObject(measurementTypesForPreviews.get(1));
-                itemPageOffset = 1;
-                break;
-            case R.id.preview2:
-                PatientSingleton.getInstance().setCurrentObject(measurementTypesForPreviews.get(2));
-                itemPageOffset = 2;
-                break;
-        }
-
-        Intent intent = new Intent(this, HelperSingleton.getInstance().getMeasurementTypeClass());
-        intent.putExtra("itemIndex",pageNumber*textPreviewsPerPage + itemPageOffset);
-        startActivity(intent);
-    }
-
-    public void goToPreviousPage(View view) {
-        if (pageNumber > 0) {
-            Intent intent = new Intent(this, MeasurementViewActivity.class);
-            intent.putExtra("pageNumber", pageNumber - 1);
-            startActivity(intent);
-        }
-    }
-
-    public void goToNextPage(View view) {
-        if ((pageNumber+1)*textPreviewsPerPage <= PatientSingleton.getInstance().getJournalEntries().length() - 1) {
-            Intent intent = new Intent(this, MeasurementViewActivity.class);
-            intent.putExtra("pageNumber", pageNumber + 1);
-            startActivity(intent);
-        }
     }
 
 }
